edba2b32b273f662ef73b7e7b228db5c474dc8bf
Ken Paulsen @ NKF <ken.paulsen@c-k.no>
2012-04-25 08:33:55 +0200

    finished file uploader example as well

--- preparation/blog/step_1/resources/config.php
+++ preparation/blog/step_1/resources/config.php
@@ -4,7 +4,9 @@
 	$config['db_user'] = 'root';
 	$config['db_pass'] = 'root';
 	$config['db_name'] = 'blog';
-	$config['site_name'] = 'My Super Special Blog'; // Just for fun
+//	$config['db_user'] = 'ckpaulse_interak';
+//	$config['db_pass'] = 'samhandling';
+//	$config['db_name'] = 'ckpaulse_blog';
 	
 	foreach ($config as $key => $value) {
 		define( strtoupper($key), $value );
---
+++ preparation/upload/file_upload/index.php
@@ -0,0 +1,96 @@
+<?php
+
+	// Printing images with details in $_FILES array
+	// This is a multidimensional array
+	//	if (!empty($_FILES)) {
+	//		print_r($_FILES);
+	//		
+	//	}
+	
+	// Has images been inserted into array?
+	if (isset($_FILES['image'])) {
+		
+		// List errors in images
+		$errors = array();
+		
+		// List of allowed file extensions
+		$allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
+		
+		// Retrieve file name from image
+		$file_name = $_FILES['image']['name'];
+
+		// Extension of image file
+		// Explode file name by '.' into an array
+		// $file_ext = explode('.', $file_name);
+		
+		// Then select last element in array
+		// $file_ext = end(explode('.', $file_name));
+		
+		// Then downcase the extension to match the $allowed_ext array
+		$file_ext = strtolower(end(explode('.', $file_name)));
+
+		// Check what is pulled as filename
+		// Test both with '.jpg' and '.JPG'
+		// print_r($file_ext);
+
+		// Size of image file
+		$file_size = $_FILES['image']['size'];
+
+		// File temporary location
+		$file_tmp = $_FILES['image']['tmp_name'];
+		
+		// Check if $file_ext is not in $allowed_ext array
+		if ( !in_array($file_ext, $allowed_ext) ) { // in_array() === false
+			// Adding a message to the $errors array
+			$errors[] = 'Extension not allowed';
+		}
+		
+		// Now test with 'test.bmp'
+		
+		// Test size against 2MB (in bytes)
+		if ( $file_size > 2097152 ) {
+			$errors[] = 'File size must be less than 2MB';
+		}
+		
+		// Now test with both 'cat_in_bag.jpg' and 'test.bmp'
+		// allowing both errors to be produced
+		// print_r($errors);
+		
+		// Validate no errors have occured
+		if (empty($errors)) {
+			// Move uploaded file to 'images' folder
+			// move_uploaded_file($file_tmp, 'images/' . $file_name);
+			
+			// Validate that upload and file move worked
+			if (move_uploaded_file($file_tmp, 'images/' . $file_name)) {
+				echo 'File uploaded';
+			}
+		} else {
+			// Print errors
+			foreach ($errors as $error) {
+				echo $error, '<br />';
+			}
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head></head>
+
+<body>
+
+	<form method="POST" action="" enctype="multipart/form-data">
+	
+		<p>
+			<input type="file" name="image" />
+			<input type="submit" value="upload" />
+		</p>
+	
+	</form>
+
+</body>
+
+</html>
